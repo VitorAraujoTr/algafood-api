4c9bd6c85da8b9915a89f97f6b68ed5e8e469e7e
Vitor Araújo <vitoraraujotr@gmail.com>
2022-06-20 21:09:44 -0300

    2.8 - Restart mais rápido da aplicação com DevTools.

--- src/main/java/com/algaworks/algafoodapi/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafoodapi/MeuPrimeiroController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MeuPrimeiroController {
 
+
+    // Para adicionar DevTools ao IntelliJ IDE
+    // - Adicionar ao pom a dependência;
+    // - File > Settings > Build, Execution, Deployment > Compiler, ativar a opção "Build project automatically";
+    // - File > Settings > Advanced Settings > ativar a opção "Allow auto-make to start...".
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "Hello!";
+        return "Hello!!!";
     }
 }
