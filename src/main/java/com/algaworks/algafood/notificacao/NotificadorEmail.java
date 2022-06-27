05f2aba57e8cfa9689bee96ebe955c885e3dafc0
Vitor Araújo <sllokstuning@gmail.com>
2022-06-27 18:54:38 -0300

    2.18 - Desambiguação de beans com @Qualifier.

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,9 +1,10 @@
 package com.algaworks.algafood.notificacao;
 
 import com.algaworks.algafood.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-//@Primary
+@Qualifier("urgente")
 @Component
 public class NotificadorEmail implements Notificador{
 
--- src/main/java/com/algaworks/algafood/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorSMS.java
@@ -1,10 +1,10 @@
 package com.algaworks.algafood.notificacao;
 
 import com.algaworks.algafood.modelo.Cliente;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Primary
+@Qualifier("normal")
 @Component
 public class NotificadorSMS implements Notificador{
 
