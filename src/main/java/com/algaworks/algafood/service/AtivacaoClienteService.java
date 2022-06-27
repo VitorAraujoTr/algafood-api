b0cfe57663cb3cfa88a361ad6758ceb03ea4c29a
Vitor Araújo <sllokstuning@gmail.com>
2022-06-26 22:38:47 -0300

    2.14 - Conhecendo os pontos de injeção e a anotação @Autowired.

--- src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -2,19 +2,45 @@ package com.algaworks.algafood.service;
 
 import com.algaworks.algafood.modelo.Cliente;
 import com.algaworks.algafood.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
-    private final Notificador notificador;
+//    Método simples de se injetar uma classe, porém, pode ser problemática
+//      por conta do seu nível de acesso privado, do qual não será enxergada
+//      principalmente em pelas classes de teste ou em uma configuração programática,
+//      que precisaria de programar uma injeção.
+    @Autowired
+    private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
+//    O Spring consegue enxergar quem é o Autowired, pois assim, você consegue
+//      apontar quem será o construtor principal da injeção.
+//
+//    O ideal seria este meio de injeção, direto no construtor, pois detalha
+//      melhor o que está utilizando e passando, e também quando for montar
+//      as classes de teste, por ser um método publico.
+//
+//    @Autowired
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
+//
+//    public AtivacaoClienteService(String qualquer) {
+//
+//    }
 
-        System.out.println("AtivacaoClienteService: " + notificador);
-    }
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
+
+//    Este método de injeção também poderá funcionar.
+//
+//    @Autowired
+//    public void setNotificador(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 }
