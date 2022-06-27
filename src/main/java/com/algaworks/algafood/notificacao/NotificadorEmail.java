456eb8a444e7cffd0e8fc4ade6ca4346ca5e26bc
Vitor Araújo <sllokstuning@gmail.com>
2022-06-27 19:41:54 -0300

    2.20 - Mudando o comportamento da aplicação com Spring Profiles.

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -1,17 +1,11 @@
 package com.algaworks.algafood.notificacao;
 
 import com.algaworks.algafood.modelo.Cliente;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador{
-
-    public NotificadorEmail() {
-        System.out.println("NotificadorEmail REAL");
-    }
+public class NotificadorEmail implements Notificador {
 
     public void notificar(Cliente cliente, String msg) {
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
--- src/main/java/com/algaworks/algafood/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorSMS.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @TipoDoNotificador(NivelUrgencia.URGENTE)
 @Component
-public class NotificadorSMS implements Notificador{
+public class NotificadorSMS implements Notificador {
 
     public void notificar(Cliente cliente, String msg) {
         System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
