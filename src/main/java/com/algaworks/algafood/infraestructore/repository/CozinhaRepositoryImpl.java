426e96a7c29dd9c83007e01ae5a45f2c5297125c
Vitor Araújo <sllokstuning@gmail.com>
2022-07-20 22:12:09 -0300

    3.14. Conhecendo e implementando o padrão Repository.

---
+++ src/main/java/com/algaworks/algafood/infraestructore/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.algaworks.algafood.infraestructore.repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public List<Cozinha> todas() {
+        return em.createQuery("from Cozinha", Cozinha.class)
+                .getResultList();
+    }
+
+    @Override
+    public Cozinha porId(Long id) {
+        return em.find(Cozinha.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Cozinha adicionar(Cozinha cozinha) {
+        return em.merge(cozinha);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Cozinha cozinha) {
+
+        cozinha = porId(cozinha.getId());
+
+        em.remove(cozinha);
+    }
+}
