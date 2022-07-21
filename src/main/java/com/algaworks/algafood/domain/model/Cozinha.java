1736f37582ea93fa4bf4653117d1e879e05cbc8f
Vitor Araújo <sllokstuning@gmail.com>
2022-07-20 22:46:03 -0300

    3.15. Conhecendo e usando o Lombok.

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,9 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,12 +8,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cozinha {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
