bf2795f08a4a0049d17358229bab6c80282aecef
Vitor Araújo <sllokstuning@gmail.com>
2022-07-20 22:53:44 -0300

    3.16. Desafio Lombok e repositório de restaurantes.

---
+++ src/main/java/com/algaworks/algafood/infraestructore/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.algaworks.algafood.infraestructore.repository;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public List<Restaurante> todos() {
+        return em.createQuery("from Restaurante", Restaurante.class)
+                .getResultList();
+    }
+
+    @Override
+    public Restaurante porId(Long id) {
+        return em.find(Restaurante.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Restaurante adicionar(Restaurante restaurante) {
+        return em.merge(restaurante);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Restaurante restaurante) {
+
+        restaurante = porId(restaurante.getId());
+
+        em.remove(restaurante);
+    }
+}
