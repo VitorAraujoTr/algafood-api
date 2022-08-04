fedb229b36aa0e2517d1315504d7599a84494346
Vitor Araújo <sllokstuning@gmail.com>
2022-08-03 23:54:47 -0300

    4.26. Modelando e implementando a exclusão de recursos com DELETE.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,9 +5,11 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,6 +66,23 @@ public class CozinhaController {
                     .body(cozinhaRepository.adicionar(cozinhaAtual));
         }
         return ResponseEntity.notFound().build();
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
+        try {
+            Cozinha cozinha = cozinhaRepository.buscarPorId(id);
+
+            if (cozinha != null) {
+                cozinhaRepository.remover(cozinha);
+
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.notFound().build();
+
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
