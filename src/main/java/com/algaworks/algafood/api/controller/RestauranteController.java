56f1ba3599313ce5f596479cc47f5506371c3306
Vitor Araújo <sllokstuning@gmail.com>
2022-08-08 22:15:20 -0300

    4.31. Desafio Modelando e implementando a atualização de recursos de restaurantes.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -5,11 +5,13 @@ import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,24 @@ public class RestauranteController {
             return ResponseEntity
                     .status(HttpStatus.CREATED)
                     .body(restauranteService.salvar(restaurante));
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Restaurante restaurante) {
+        try {
+            Restaurante restauranteAtual = restauranteRepository.porId(id);
+
+            if (restaurante != null) {
+                BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+
+                return ResponseEntity.ok(restauranteService.salvar(restaurante));
+            }
+            return ResponseEntity.notFound().build();
         } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity
                     .badRequest()
